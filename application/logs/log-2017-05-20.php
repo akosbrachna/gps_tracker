4528df7e0ac0029e7006738e55b1508c215e4f59
akosbrachna <akos.brachna@gmail.com>
2017-05-20 15:05:27 +0100

    first commit

---
+++ application/logs/log-2017-05-20.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2017-05-20 13:45:40 --> Severity: Notice  --> Undefined index: first_login C:\xampp\htdocs\gps_tracker\application\models\authorization\Login_model.php 50
